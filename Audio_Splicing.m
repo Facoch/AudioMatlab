d29ef7f36edf9965a49e907e906a5fa40240a8d9
davidepiona <davide.piona@studenti.unipd.it>
2016-04-07 16:53:33 +0200

    First commit, added the first version of program

---
+++ Audio_Splicing.m
@@ -0,0 +1,85 @@
+clear all;  %pulisco il workspace
+close all;
+clc;
+
+
+file= '3Campioni_Tagliati_96.wav';
+[y,Fs] = audioread(file);
+info = audioinfo(file);
+
+
+T=1/32000;
+f=0:1:32000;
+taudio = T*fft(y, length(f));
+
+t = 0:seconds(1/Fs):seconds(info.Duration);
+t = t(1:end-1);
+
+left=y(:,1); % Left channel 
+
+%subplot (3,1,1);    plot(f,20*log10(abs(taudio)));
+%subplot (3,1,2);    plot(t,y);
+NUMEL = round(numel(left)/1000);
+NUMELOVER = round(50/100*NUMEL);
+
+[s,f,t,ps]= spectrogram(left,NUMEL,NUMELOVER,[],Fs,'yaxis');
+
+spectrogram(left,NUMEL,NUMELOVER,[],Fs,'yaxis', 'MinThreshold',-200);
+%xlim([1.1 1.4]); 
+
+
+%ricerca precisa estremi 8'000 - 40'000
+DIMF=size(f);
+FROM=round(8000*DIMF(1)/f(DIMF(1)));
+while round(f(FROM))>8000
+        FROM=FROM-1;
+end
+while round(f(FROM))<8000
+        FROM=FROM+1;
+end
+
+TO=round(40000*DIMF(1)/f(DIMF(1)));
+while round(f(TO))>40000
+        TO=TO-1;
+end
+while round(f(TO))<40000
+        TO=TO+1;
+end    
+    
+
+DIMT = size(t);
+COUNT=1;
+STOT=0;
+
+for j=1:DIMT(2)
+    
+    S=0;
+    for i=FROM:TO
+        S = S+ps(i,j);
+        STOT = STOT+ps(i,j);
+    end
+    pw=db(S/(1+TO-FROM));    % pw=10*log(S/(TO-FROM)); 
+    if(-240<pw)
+        disp('Alto');
+         Numero(COUNT)=COUNT;
+         Tempo(COUNT)=fix(t(j)/60)+(t(j)-fix(t(j)/60)*60)/100;
+         Valore(COUNT)=pw;
+        COUNT=COUNT+1;
+        end
+end
+    
+T=table(Numero', Tempo', Valore');
+disp(T);
+
+disp('STOT');
+disp(db(STOT/((1+TO-FROM)*(DIMT(2)))));
+
+
+
+%PROSSIME COSE: 
+%           capire suddivisione brano
+%           trovare valori base riconoscimento (quindi non comparativo interno)
+%           trovare modo per info generali sul file audio/pezzo di brano
+
+
+
