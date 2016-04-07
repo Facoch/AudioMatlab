0caba2d2318ee7b4ae0af41aa4f8f0816d9feab7
davidepiona <davide.piona@studenti.unipd.it>
2016-04-07 22:20:29 +0200

    +Added the possibility to check both the channels +added the tables fusion in one big table +changed the way to define spectrogram params

--- Audio_Splicing.m
+++ Audio_Splicing.m
@@ -6,22 +6,50 @@ clc;
 file= 'Campioni_Tagliati_96_PrimaParte.wav';
 clear y Fs
 [y,Fs] = audioread(file);
-duration = length(y)/Fs;
 
-%split the file in more parts 40 seconds each (more precision)
-if duration>40
-    for i=1:(1+duration/40)
-        split=y(1+Fs*40*(i-1) : Fs*40*i);
-        left=split(1,:);
-        x = analizeSplit(left, Fs, i-1); 
-        %here I can use x table ... 
+%inizialize some variables
+duration = length(y)/Fs;
+Numero=[];
+Tempo=[];
+Valore=[];
+h=table(Numero', Tempo', Valore','VariableNames',{'Numero' 'Tempo' 'Valore'});
+interval=20;
+
+%split the file in more parts 20 seconds each (more precision)
+%left channel
+if duration>interval
+    for i=1:(1+duration/interval)
+        split=y(1+Fs*interval*(i-1) : Fs*interval*i);
+        x = analizeSplit(split, Fs, i-1, interval);
+        h=[h; x];
+        
     end
 end
-if duration<40
-    x= analizeSplit(y,Fs,0, duration);
-    
+if duration<interval
+    split=y(1: Fs*duration);
+    x= analizeSplit(split,Fs,0, interval);
+    h=[h; x]; 
+end
+disp(h);
+
+%right channel
+y1=y;
+y1(:,[1 2])=y1(:,[2 1]);
+g=table(Numero', Tempo', Valore','VariableNames',{'Numero' 'Tempo' 'Valore'});
+if duration>interval
+    for i=1:(1+duration/interval)
+        split=y1(1+Fs*interval*(i-1) : Fs*interval*i);
+        x = analizeSplit(split, Fs, i-1, interval); 
+        g=[g; x];
+    end
+end
+if duration<interval
+    split=y1(1: Fs*duration);
+    x= analizeSplit(split,Fs,0, interval);
+    g=[g; x]; 
 end
 
+disp(g);
 
 
 
--- analizeSplit.m
+++ analizeSplit.m
@@ -1,28 +1,30 @@
 %this function returns the table 'x' containing the information about the
 %potential cuts on my audio file
-function [x]= analizeSplit(audio,Fs, k)
+function [x]= analizeSplit(audio,Fs, k, interval)
 
 
-NUMEL = round(numel(audio)/1000);
-NUMELOVERLAP = round(50/100*NUMEL);
+
+%define FFT parameters
+des_df_Hz = 25;  %desired frequency resolution for the display, Hz
+Nfft= 1.6*round(round(Fs / des_df_Hz));  %general rule for FFT resolution %1.6 is empirical
 
 %generate the spectogram's matrices
-[s,f,t,ps]= spectrogram(audio,NUMEL,NUMELOVERLAP,[],Fs,'yaxis');
+[s,f,t,ps]=spectrogram(audio, Nfft, round(0.5*Nfft), Nfft, Fs,'yaxis');
 %draw spectograms
 figure(k+1);
-spectrogram(audio,NUMEL,NUMELOVERLAP,[],Fs,'yaxis', 'MinThreshold',-200);
+spectrogram(audio, Nfft, round(0.4*Nfft), Nfft, Fs,'yaxis','MinThreshold',-200);
 %xlim([1.1 1.4]); 
 
 %search index of 15,20,40 kHz (FROM, TO1, TO2)
-FROM=searchFrequencyIndex(f,15000);
+FROM=searchFrequencyIndex(f,13000);
 TO1=searchFrequencyIndex(f,20000);
 TO2=searchFrequencyIndex(f,40000);
    
 
 DIMT = size(t);
-%il seguente ciclo for è solo per cercare di avere una 'media' dei valori
-%così da impostare il 'valore soglia' nel prossimo ciclo for
-%questa parte è da migliorare molto!
+% il seguente ciclo for è solo per cercare di avere una 'media' dei valori
+% così da impostare il 'valore soglia' nel prossimo ciclo for
+% questa parte è da migliorare molto!
 STOT=0;
 COUNT=0;
 for j=1:DIMT(2)         %tempi del vettore t
@@ -43,9 +45,9 @@ COUNT=1;
 %del 'valore soglia' quell'istante della canzone è un possibile taglio sul 
 %nastro quindi ne salvo le informazioni in Numero, Tempo e Valore.
 
-Numero(1)=1;
-Tempo(1)=1;
-Valore(1)=1;
+Numero=[];
+Tempo=[];
+Valore=[];
 for j=1:DIMT(2)         %tempi del vettore t
     S=0;
     for i=FROM:TO2      %frequenza da FROM a TO2
@@ -53,7 +55,7 @@ for j=1:DIMT(2)         %tempi del vettore t
     end
     pw=db(S/(1+TO2-FROM));    
     if(MEDTOT+5<pw)         %confronto con valore soglia (MEDTOT è la media mentre 5 è un valore empirico
-         time= t(j)+40*k;
+         time= t(j)+interval*k;
          Numero(COUNT)=COUNT;
          Tempo(COUNT)=fix(time/60)+((time-fix(time/60)*60)/100);
          Valore(COUNT)=pw;
@@ -63,10 +65,10 @@ end
 
 %print a table that collects 3 vectors and some other interesting values
 [MAX,I]=max(Valore);
-fprintf('\n\n---------------------------\n');
+
 x=table(Numero', Tempo', Valore','VariableNames',{'Numero' 'Tempo' 'Valore'});
-disp(x);
 fprintf('Media= %.2f   Media tutti=%.2f\nMAX= %.2f  at Time= %.4f\n',mean(Valore),MEDTOT, MAX, Tempo(I));
+fprintf('\n---------------------------\n');
 
 
 
